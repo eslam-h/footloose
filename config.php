ae2e5060ac31ab98d87d533e7b2732f68718a07d
root <root@geekubuntu16.c.footloose-173110.internal>
2018-08-01 17:54:40 +0000

    second push

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+define('HTTP', $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']));
+// HTTP
+//define('HTTP_SERVER', 'http://footloose.thegeeksolutions.com/');
+define('HTTP_SERVER', 'http://'.HTTP);
+// HTTPS
+//define('HTTPS_SERVER', 'http://footloose.thegeeksolutions.com/');
+define('HTTPS_SERVER', 'http://'.HTTP);
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/footlooseprod/catalog/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/footlooseprod/system/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/footlooseprod/image/');
+define('DIR_LANGUAGE', '/opt/lampp/htdocs/footlooseprod/catalog/language/');
+define('DIR_TEMPLATE', '/opt/lampp/htdocs/footlooseprod/catalog/view/theme/');
+define('DIR_CONFIG', '/opt/lampp/htdocs/footlooseprod/system/config/');
+define('DIR_CACHE', '/opt/lampp/htdocs/footlooseprod/system/storage/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/htdocs/footlooseprod/system/storage/download/');
+define('DIR_LOGS', '/opt/lampp/htdocs/footlooseprod/system/storage/logs/');
+define('DIR_MODIFICATION', '/opt/lampp/htdocs/footlooseprod/system/storage/modification/');
+define('DIR_UPLOAD', '/opt/lampp/htdocs/footlooseprod/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'footloose_prod');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
